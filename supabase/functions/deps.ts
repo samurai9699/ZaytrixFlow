ebd83ab177d4ff8272bfc9b02f27f05604af98f2
Sam Muriithi <105632389+samurai9699@users.noreply.github.com>
2025-06-09 21:18:36 +0300

    Updated deps.ts

---
+++ supabase/functions/deps.ts
@@ -0,0 +1,3 @@
+// @deno-types="https://deno.land/std@0.208.0/http/server.ts"
+export { serve } from "https://deno.land/std@0.208.0/http/server.ts";
+export { createClient } from "https://esm.sh/@supabase/supabase-js@2.39.7?dts"; 
